aba56a8fec4cf6bb3025004c4d5fead8a6038f5e
Are You Serious? <35229666+crajygemer@users.noreply.github.com>
2020-06-26 11:36:32 +0600

    Update settings.py

--- ten_project/ten_project/settings.py
+++ ten_project/ten_project/settings.py
@@ -22,7 +22,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '62et9$-lop)l42mx=4*1gvuo@j(gg9d(et$!*g500mmakv7ld&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
